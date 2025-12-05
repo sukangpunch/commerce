7c8f56b780239021b662be411fb3beff92b61379
sukangpunch <popora99@naver.com>
2025-12-05 23:48:22 +0900

    feat: 서비스, 리포지토리 클래스 초기화

---
+++ src/main/kotlin/com/example/commerce/user/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.commerce.user.repository
+
+import com.example.commerce.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+}
